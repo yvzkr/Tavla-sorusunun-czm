13c4e0de072033b7c50e70556c860c6b0c5b0851
yvzkr <yavuzkuru3@gmail.com>
2019-07-10 16:41:28 +0300

    konumlama ve puanlama fonksiyonu yazıldı.

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+def konumlamaVePuanlama(konum1,konum2,zar1,zar2,checkers):
 
+    pass
 
 def find_moves(checkers, dice1, dice2):
     #checkers={1: 3, 6: 1, 10: 2, 12: 1, 19: 1}
@@ -16,7 +18,20 @@ def find_moves(checkers, dice1, dice2):
     for i in taslar:
         #sonra j ye de  sırasıyla
         for j in taslar:
-            print(i,j)
+            #bütün taşların birbirleriyle ikişer ikişer eşleşerek konumlama ve Puanlama için
+            #konumlama ve puanlama fonksiyonuna gönderildi.
+            #fonksiyonda False Dönme olasılıkları var bunlar taşların haraket edemez olduğunu gösterir
+            #örnegin 24. üçgendeki taş hiç haraket edemez. Çünkü gelebileceği en uç noktaya gelmiştir
+            sonuc=konumlamaVePuanlama(i,j,dice1,dice2,checkers)
+            #sonuc eğer False değilse puanTablosuListesine eklenir
+            if sonuc:
+                puanTablosuListesi.append(sonuc)
+
+    #puan tablosu listesi ilk set yapılıyor çünkü burada bazı aynı konumdaki taşlarda çalıştığı için
+    #bunları burada eledik. Örneğin 1 . yuvada 3 tane var tavlada sadece en üstteki taş haraket ettiriliyordu
+    #3 defa oynattığımız için aynı taşı iki 3 defa oynatmış oluyoruz. aynı saonuçtan 3 tane olmuş oluyor
+    #3 tane sonuçtan bir tanesi bize yeterli olduğu için sadece bir tanesini alsakk bize yeterli olur.
+    return list(set(puanTablosuListesi))
 
 
 
