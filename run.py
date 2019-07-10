a6a0c93ebdf5a2f04e43926185a8a6b2a4807c72
yvzkr <yavuzkuru3@gmail.com>
2019-07-10 07:37:07 +0300

    tasları her bir birleri arasında dönebilmeleri için bir listeye atıldılar

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
+
 def find_moves(checkers, dice1, dice2):
-    pass
+
+    #bütün taşları konumsal olarak taslar listesine atar
+    taslar=[konum for konum,tasSayisi in checkers.items() for i in range(tasSayisi) ]
+
+    print("Taşlar:",taslar)
+    print("cherkers: ", checkers)
 
 
 
-checkers = {1: 3, 6: 1, 10: 2, 12: 1, 13: 1}
+
+checkers = {1: 3, 6: 1, 10: 2, 12: 1, 19: 1}
 print(find_moves(checkers, 6, 1))
